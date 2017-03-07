5e7b43d58cdc5ed7f33de678b4c1da0625084eec
Keenan Louis <klouis13@georgefox.edu>
2017-03-06 18:26:55 -0800

    Formatting

--- src/FieldOccupant.java
+++ src/FieldOccupant.java
@@ -4,10 +4,10 @@ import java.awt.Color;
  * Abstract parent class for objects that can occupy a cell in the Field
  */
 public abstract class FieldOccupant
-{ 
+{
    /**
     * @return the color to use for a cell containing a particular kind
-    *         of occupant
+    * of occupant
     */
    abstract public Color getDisplayColor();
 }
